2bc8d07689470cc8d10d64d4fef7c7921eb6dfb9
vladbv <vladimirbhelp@gmail.com>
2022-07-26 17:24:09 +0300

    Added Problem 69

---
+++ problems-61-80/problem-69/script.js
@@ -0,0 +1,40 @@
+// Problem 69
+// This problem was asked by Apple.
+//
+// Suppose you have a multiplication table that is N by N. That is, a 2D array where the value at the i-th row and j-th column is (i + 1) * (j + 1) (if 0-indexed) or i * j (if 1-indexed).
+//
+// Given integers N and X, write a function that returns the number of times X appears as a value in an N by N multiplication table.
+//
+// For example, given N = 6 and X = 12, you should return 4, since the multiplication table looks like this:
+//
+// | 1 | 2 | 3 | 4 | 5 | 6 |
+//
+// | 2 | 4 | 6 | 8 | 10 | 12 |
+//
+// | 3 | 6 | 9 | 12 | 15 | 18 |
+//
+// | 4 | 8 | 12 | 16 | 20 | 24 |
+//
+// | 5 | 10 | 15 | 20 | 25 | 30 |
+//
+// | 6 | 12 | 18 | 24 | 30 | 36 |
+//
+// And there are 4 12's in the table.
+//
+// O(N) Time complexity
+// O(1) Space complexity
+
+// Returning the number of times X appears as a value by an N by N multiplication table
+// @param {int} n
+// @param {int} x
+// @return {int}
+
+function multiplicationTable(n, x) {
+    let count = 0;
+    for(let i = 1; i <= n; i++){
+        if(x % 1 === 0 && x / i <= n) {
+            count++;
+        }
+    }
+    return count;
+}
