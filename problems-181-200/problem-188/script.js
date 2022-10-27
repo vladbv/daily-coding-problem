f2d1439f3cc97a7016c82b84b6fdbbfa38a8cb2c
vladbv <vladimirbhelp@gmail.com>
2022-10-27 21:53:24 +0300

    Added Problem 188

---
+++ problems-181-200/problem-188/script.js
@@ -0,0 +1,43 @@
+/* Problem 188
+
+This problem was asked by Amazon.
+
+Given a pivot x, and a list lst, partition the list into three parts.
+
+The first part contains all elements in lst that are less than x The second part contains all elements in lst that are equal to x The third part contains all elements in lst that are larger than x Ordering within a part can be arbitrary.
+
+For example, given x = 10 and lst = [9, 12, 3, 5, 14, 10, 10], one partition may be [9, 3, 5, 10, 10, 12, 14].
+
+*/
+
+/* Partitioning a list into three parts
+ * @param {number[]} list
+ * @param {number} x
+ * @return {number[]}
+ */
+
+function partitionList(list, x) {
+	
+  const lo = [];
+  const eq = [];
+  const hi = [];
+  
+  // iterating through list
+  
+  for(let i = 0; i < list.length; i++){
+		if(list[i] === x){
+    	eq.push(list[i]);
+    } else if(list[i] < x) {
+    	lo.push(list[i])
+    } else {
+    	hi.push(list[i])
+    }
+  }
+  
+  
+  const partition = [...lo, ...eq, ...hi];
+  return partition;
+  
+}
+console.log(partitionList([9, 12, 3, 5, 14, 10, 10], 10));
+
